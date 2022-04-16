452ff6b8a910d54e29bda29539f12e56dc480be4
20071a6931 <101632126+20071a6931@users.noreply.github.com>
2022-04-16 10:31:02 +0530

    Add files via upload

---
+++ EvenOrOdd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class EvenOrOdd
+{
+public static void main(String args[])
+{
+Scanner read=new Scanner(System.in);
+int n=read.nextInt();
+if(n%2==0)
+{
+System.out.println("The given number is even number");
+}
+else
+{
+System.out.println("The given number is odd number");
+}
+}
+}
